023f1edc7087f59f839968508570b6786ee8bc34
MinChan Lee <shigatsu970704@gmail.com>
2021-05-17 14:37:34 +0900

    02.Apollo GraphQL Set Up

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client/react";
 import App from "./App";
+import { client } from "./apollo";
 import reportWebVitals from "./reportWebVitals";
 import "./styles/styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );

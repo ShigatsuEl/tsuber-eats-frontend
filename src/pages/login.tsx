a9ac161cd20e71f0edbdc69b81649dc306858051
MinChan Lee <shigatsu970704@gmail.com>
2021-05-18 13:59:26 +0900

    07.Router and @types
    Fix Dependency autoprefixer & postcss from Devdependency

---
+++ src/pages/login.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const LogIn = () => {
+  return <span>Log In</span>;
+};
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,53 +1,19 @@
 import React from "react";
-import { useForm } from "react-hook-form";
-import { isLoggedInVar } from "../apollo";
-
-interface IForm {
-  email: string;
-  password: string;
-}
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CreateAccount } from "../pages/create-account";
+import { LogIn } from "../pages/login";
 
 export const LoggedOutRouter = () => {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IForm>();
-  const onValid = () => console.log(watch("email"));
-  const onInValid = () => console.log("Can't create account");
   return (
-    <div>
-      <h1>Logged Out</h1>
-      <form onSubmit={handleSubmit(onValid, onInValid)}>
-        <div>
-          <input
-            {...register("email", {
-              required: "This is required",
-              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
-            })}
-            type="email"
-            name="email"
-            placeholder="email"
-          />
-          {errors.email?.message && (
-            <span className="text-red-600">{errors.email.message}</span>
-          )}
-          {errors.email?.type === "pattern" && (
-            <span className="text-red-600">Only gmail allowed</span>
-          )}
-        </div>
-        <div>
-          <input
-            {...register("password", { required: true })}
-            type="password"
-            name="password"
-            placeholder="password"
-            required
-          />
-        </div>
-        <button>Submit</button>
-      </form>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <LogIn />
+        </Route>
+      </Switch>
+    </Router>
   );
 };

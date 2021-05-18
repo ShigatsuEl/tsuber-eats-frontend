d0d305871dc1e899e5091704324089e72c64446d
MinChan Lee <shigatsu970704@gmail.com>
2021-05-18 13:30:13 +0900

    05.React Hook Form

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useReactiveVar } from "@apollo/client";
 import React from "react";
 import { isLoggedInVar } from "./apollo";
 import { LoggedInRouter } from "./routers/logged-in-router";
-import { LoggedOutrRouter } from "./routers/logged-out-router";
+import { LoggedOutRouter } from "./routers/logged-out-router";
 
 function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
-  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutrRouter />;
+  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 }
 
 export default App;
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { isLoggedInVar } from "../apollo";
 
-export const LoggedOutrRouter = () => {
+export const LoggedOutRouter = () => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onValid = () => console.log(watch("email"));
+  const onInValid = () => console.log("Can't create account");
+  console.log(errors);
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={() => isLoggedInVar(true)}>Click to Login</button>
+      <form onSubmit={handleSubmit(onValid, onInValid)}>
+        <div>
+          <input
+            {...register("email", {
+              required: "This is required",
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            type="email"
+            name="email"
+            placeholder="email"
+          />
+        </div>
+        <div>
+          <input
+            {...register("password", { required: true })}
+            type="password"
+            name="password"
+            placeholder="password"
+            required
+          />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 };

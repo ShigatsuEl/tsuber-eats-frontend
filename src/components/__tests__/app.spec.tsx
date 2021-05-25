2ebd35d875d11bed7d0a39b099fc934d3fcebb64
MinChan Lee <shigatsu970704@gmail.com>
2021-05-25 10:46:12 +0900

    49.App Component Test

---
+++ src/components/__tests__/app.spec.tsx
@@ -0,0 +1,32 @@
+import { render, waitFor } from "@testing-library/react";
+import React from "react";
+import { isLoggedInVar } from "../../apollo";
+import App from "../app";
+
+jest.mock("../../routers/logged-out-router", () => {
+  return {
+    LoggedOutRouter: () => <span>Logged Out</span>,
+  };
+});
+
+jest.mock("../../routers/logged-in-router", () => {
+  return {
+    LoggedInRouter: () => <span>Logged In</span>,
+  };
+});
+
+describe("<App />", () => {
+  it("renders LoggedOutRouter", () => {
+    const { getByText } = render(<App />);
+    getByText("Logged Out");
+  });
+
+  it("renders LoggedInRouter", async () => {
+    const { debug, getByText } = render(<App />);
+    await waitFor(() => {
+      isLoggedInVar(true);
+    });
+    debug();
+    getByText("Logged In");
+  });
+});

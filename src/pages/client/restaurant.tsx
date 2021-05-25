7d61ccbd853c8d8432c606c1a35fd59c4ac30ea5
MinChan Lee <shigatsu970704@gmail.com>
2021-05-25 18:40:57 +0900

    47.Restaurant Page(UI)

--- src/pages/client/restaurant.tsx
+++ src/pages/client/restaurant.tsx
@@ -1,7 +1,11 @@
 import { useQuery } from "@apollo/client";
+import { faUtensils } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import gql from "graphql-tag";
 import React from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import Loading from "../../components/loading";
 import { RESTAURANT_FRAGMENT } from "../../fragments";
 import {
   GetRestaurantQuery,
@@ -38,5 +42,38 @@ export const Restaurant = () => {
     },
   });
   console.log(!loading && data);
-  return <h1>Restaurant</h1>;
+  return (
+    <React.Fragment>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <div
+            className="relative py-40 px-10 bg-cover bg-center bg-gray-800"
+            style={{
+              backgroundImage: `url(${data?.getRestaurant.restaurant?.coverImg})`,
+            }}
+          >
+            <div className="absolute inset-x-0 bottom-0 px-10 pb-5 text-white bg-gradient-to-t from-black">
+              <h4 className="pb-2 font-semibold text-4xl">
+                {data?.getRestaurant.restaurant?.name}
+              </h4>
+              <span className="group">
+                <FontAwesomeIcon icon={faUtensils} />
+                <Link
+                  to={`/category/${data?.getRestaurant.restaurant?.category?.name}`}
+                  className="mx-3 capitalize group-hover:text-lime-600"
+                >
+                  {data?.getRestaurant.restaurant?.category?.name}
+                </Link>
+              </span>
+              <h5 className="flex items-center text-xl">
+                {data?.getRestaurant.restaurant?.address}
+              </h5>
+            </div>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
+  );
 };

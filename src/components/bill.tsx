a038fab322e2f63b7de5cfca9a34d817de1edafe
MinChan Lee <shigatsu970704@gmail.com>
2021-06-10 12:02:34 +0900

    113.Bill Component CSS

--- src/components/bill.tsx
+++ src/components/bill.tsx
@@ -55,82 +55,94 @@ export const Bill: React.FC<IBillProps> = ({ menu, orderItems }) => {
   }, [orderItems]);
 
   return (
-    <div className="m-10 p-5 text-white bg-black">
-      {orderItems?.map((order) => (
-        <div key={order.dishId} className={`flex flex-col my-5`}>
-          <h3 className="flex items-center text-3xl">
-            <span className="mr-1">
-              {menu?.find((dish) => dish.id === order.dishId)?.name}
-            </span>
-            <span>
-              ({menu?.find((dish) => dish.id === order.dishId)?.price}￦)
-            </span>
-          </h3>
-          {order.options?.map((orderOption, index) => (
-            <div key={index}>
-              <h4 className="flex items-center text-xl">
-                <span className="mr-1">
-                  {
-                    menu
-                      ?.find((dish) => dish.id === order.dishId)
-                      ?.options?.find(
-                        (option) => option.name === orderOption.name
-                      )?.name
-                  }
-                </span>
-                <span>
-                  (
-                  {
-                    menu
-                      ?.find((dish) => dish.id === order.dishId)
-                      ?.options?.find(
-                        (option) => option.name === orderOption.name
-                      )?.extra
-                  }
-                  ￦)
-                </span>
-                {menu
-                  ?.find((dish) => dish.id === order.dishId)
-                  ?.options?.find((option) => option.name === orderOption.name)
-                  ?.choices?.find(
-                    (choice) => choice.name === orderOption.choice
-                  ) && (
-                  <>
-                    <span className="mr-2">
-                      <FontAwesomeIcon className="mx-3" icon={faArrowRight} />
+    <div className="m-10 p-5 text-white bg-black lg:flex-wrap lg:bill-container">
+      {orderItems
+        ?.map((order) => (
+          <div key={order.dishId} className={`flex flex-col my-5`}>
+            <h3 className="flex flex-wrap text-3xl mb-5 leading-10">
+              <span className="mr-1">
+                {menu?.find((dish) => dish.id === order.dishId)?.name}
+              </span>
+              <span className="font-semibold">
+                (+{menu?.find((dish) => dish.id === order.dishId)?.price}￦)
+              </span>
+            </h3>
+            {order.options
+              ?.map((orderOption, index) => (
+                <div key={index}>
+                  <h4 className="flex flex-wrap text-xl mb-2 leading-10">
+                    <span className="mr-1">
                       {
                         menu
                           ?.find((dish) => dish.id === order.dishId)
                           ?.options?.find(
                             (option) => option.name === orderOption.name
-                          )
-                          ?.choices?.find(
-                            (choice) => choice.name === orderOption.choice
                           )?.name
                       }
                     </span>
-                    <span>
-                      (
+                    <span className="font-semibold">
+                      (+
                       {
                         menu
                           ?.find((dish) => dish.id === order.dishId)
                           ?.options?.find(
                             (option) => option.name === orderOption.name
-                          )
-                          ?.choices?.find(
-                            (choice) => choice.name === orderOption.choice
                           )?.extra
                       }
                       ￦)
                     </span>
-                  </>
-                )}
-              </h4>
-            </div>
-          ))}
-        </div>
-      ))}
-      <span>{total}</span>
+                    {menu
+                      ?.find((dish) => dish.id === order.dishId)
+                      ?.options?.find(
+                        (option) => option.name === orderOption.name
+                      )
+                      ?.choices?.find(
+                        (choice) => choice.name === orderOption.choice
+                      ) && (
+                      <>
+                        <span className="mr-2">
+                          <FontAwesomeIcon
+                            className="mx-3"
+                            icon={faArrowRight}
+                          />
+                          {
+                            menu
+                              ?.find((dish) => dish.id === order.dishId)
+                              ?.options?.find(
+                                (option) => option.name === orderOption.name
+                              )
+                              ?.choices?.find(
+                                (choice) => choice.name === orderOption.choice
+                              )?.name
+                          }
+                        </span>
+                        <span className="font-semibold">
+                          (+
+                          {
+                            menu
+                              ?.find((dish) => dish.id === order.dishId)
+                              ?.options?.find(
+                                (option) => option.name === orderOption.name
+                              )
+                              ?.choices?.find(
+                                (choice) => choice.name === orderOption.choice
+                              )?.extra
+                          }
+                          ￦)
+                        </span>
+                      </>
+                    )}
+                  </h4>
+                </div>
+              ))
+              .reverse()}
+          </div>
+        ))
+        .reverse()}
+      <hr className="h-5" />
+      <span className="inline-block mb-5 font-semibold text-2xl">
+        Total: {total}￦
+      </span>
     </div>
   );
 };
